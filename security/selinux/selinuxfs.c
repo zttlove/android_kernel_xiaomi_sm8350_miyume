34e8544baa1b8bd24dd37c4bfcec75d7c8260144
NikoRurQwQRuaaMeow <nikorur666mc@gmail.com>
2026-08-23 20:26:19 +0800

    security: selinux: Hack androidboot.selinux behavior in kernel Change-Id: I1358eb6e0b9f1cc8110aa6f3bf50ea6bd2cd1094 security: selinux: Hack androidboot.selinux behavior in kernel Change-Id: I1358eb6e0b9f1cc8110aa6f3bf50ea6bd2cd1094 Co-authored-by: zhaoyuenan <amktiao030215@gmail.com> Signed-off-by: NikoRur_QwQRuaa_Nya <nikorur666mc@gmail.com>

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -118,6 +118,20 @@ static void selinux_fs_info_free(struct super_block *sb)
 #define SEL_INO_MASK			0x00ffffff
 
 #define TMPBUFLEN	12
+
+#ifdef CONFIG_SECURITY_SELINUX_CMDLINE_CONTROL
+static bool default_permissive = false;
+static int __init default_selinux_setup(char *str)
+{
+	if (strcmp(str, "permissive") == 0)
+		default_permissive = true;
+	pr_info("[SELinux] Set Default SELinux status to [%s]\n",
+		default_permissive ? "Permissive" : "Enforceing");
+	return 1;
+}
+__setup("androidboot.selinux=", default_selinux_setup);
+#endif
+
 static ssize_t sel_read_enforce(struct file *filp, char __user *buf,
 				size_t count, loff_t *ppos)
 {
@@ -157,6 +171,11 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 		goto out;
 
 	new_value = !!new_value;
+
+#ifdef CONFIG_SECURITY_SELINUX_CMDLINE_CONTROL
+	if (default_permissive || strcmp(current->comm, "init") == 0)
+		new_value = 0;
+#endif
 
 	old_value = enforcing_enabled(state);
 	if (new_value != old_value) {
