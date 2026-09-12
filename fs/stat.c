189843b4b143332802156620fc188cdc0b85035c
zttlove <115808960+zttlove@users.noreply.github.com>
2026-09-12 20:40:19 +0800

    Update stat.c

--- fs/stat.c
+++ fs/stat.c
@@ -60,9 +60,6 @@ void generic_fillattr(struct inode *inode, struct kstat *stat)
 	stat->ctime = inode->i_ctime;
 	stat->blksize = i_blocksize(inode);
 	stat->blocks = inode->i_blocks;
-#ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
-	susfs_sus_kstat_spoof_generic_fillattr(inode, stat);
-#endif
 }
 EXPORT_SYMBOL(generic_fillattr);
 
