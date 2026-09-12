3cdf428fb6a55d7db6734dc53e3a83d3e0489e84
zttlove <115808960+zttlove@users.noreply.github.com>
2026-09-12 16:09:51 +0800

    Update base.c

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -2399,12 +2399,11 @@ proc_map_files_readdir(struct file *file, struct dir_context *ctx)
 	 * routine might require mmap_sem taken in might_fault().
 	 */
 
-	for (vma = mm->mmap, pos = 2; vma; vma = vma->vm_next) {
+		for (vma = mm->mmap, pos = 2; vma; vma = vma->vm_next) {
 		if (!vma->vm_file)
 			continue;
 #ifdef CONFIG_KSU_SUSFS_SUS_MAP
-		inode = file_inode(vma->vm_file);
-		if (SUSFS_IS_INODE_SUS_MAP(inode))
+		if (SUSFS_IS_INODE_SUS_MAP(file_inode(vma->vm_file)))
 			continue;
 #endif
 		if (++pos <= ctx->pos)
