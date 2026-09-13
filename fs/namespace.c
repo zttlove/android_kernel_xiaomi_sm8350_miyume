76b33a62a4b0ab471014a61a55a5fe39e860989c
zttlove <115808960+zttlove@users.noreply.github.com>
2026-09-13 09:33:31 +0800

    Update namespace.c

--- fs/namespace.c
+++ fs/namespace.c
@@ -29,6 +29,9 @@
 #include <linux/sched/task.h>
 #include <uapi/linux/mount.h>
 #include <linux/fs_context.h>
+#ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
+#include <linux/susfs_def.h>
+#endif
 #include <linux/shmem_fs.h>
 
 #include "pnode.h"
