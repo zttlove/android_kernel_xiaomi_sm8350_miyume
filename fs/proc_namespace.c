4494b8df51307111a45a6d917270629c8d7bdf43
zttlove <115808960+zttlove@users.noreply.github.com>
2026-09-14 15:06:23 +0800

    Update proc_namespace.c

--- fs/proc_namespace.c
+++ fs/proc_namespace.c
@@ -22,8 +22,8 @@
 #include "internal.h"
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-extern bool susfs_hide_sus_mnts_for_non_su_procs;
 extern bool susfs_is_current_ksu_domain(void);
+extern struct static_key_false susfs_is_hide_sus_mnts_for_non_su_procs_enabled;
 #endif
 
 static __poll_t mounts_poll(struct file *file, poll_table *wait)
@@ -111,7 +111,7 @@ static int show_vfsmnt(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (READ_ONCE(susfs_hide_sus_mnts_for_non_su_procs) &&
+	if (static_branch_unlikely(&susfs_is_hide_sus_mnts_for_non_su_procs_enabled) &&
 		r->mnt_id >= DEFAULT_KSU_MNT_ID &&
 		!susfs_is_current_ksu_domain())
 	{
@@ -156,7 +156,7 @@ static int show_mountinfo(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (READ_ONCE(susfs_hide_sus_mnts_for_non_su_procs) &&
+	if (static_branch_unlikely(&susfs_is_hide_sus_mnts_for_non_su_procs_enabled) &&
 		r->mnt_id >= DEFAULT_KSU_MNT_ID &&
 		!susfs_is_current_ksu_domain())
 	{
@@ -229,7 +229,7 @@ static int show_vfsstat(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (READ_ONCE(susfs_hide_sus_mnts_for_non_su_procs) &&
+	if (static_branch_unlikely(&susfs_is_hide_sus_mnts_for_non_su_procs_enabled) &&
 		r->mnt_id >= DEFAULT_KSU_MNT_ID &&
 		!susfs_is_current_ksu_domain())
 	{
