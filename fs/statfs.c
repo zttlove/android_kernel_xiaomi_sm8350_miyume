8fade516e91da83b34a3ee20ed3891e622efe483
zttlove <115808960+zttlove@users.noreply.github.com>
2026-09-14 19:32:29 +0800

    Remove SUSFS open redirect code from vfs_statfs
    Removed conditional compilation for SUSFS open redirect functionality in vfs_statfs.

--- fs/statfs.c
+++ fs/statfs.c
@@ -71,10 +71,6 @@ static int statfs_by_dentry(struct dentry *dentry, struct kstatfs *buf)
 	return retval;
 }
 
-#ifdef CONFIG_KSU_SUSFS_OPEN_REDIRECT
-extern int susfs_open_redirect_spoof_vfs_statfs(struct inode *inode, struct kstatfs *buf);
-#endif // #ifdef CONFIG_KSU_SUSFS_OPEN_REDIRECT
-
 int vfs_get_fsid(struct dentry *dentry, __kernel_fsid_t *fsid)
 {
 	struct kstatfs st;
@@ -123,15 +119,6 @@ bypass_orig_flow:
 int vfs_statfs(const struct path *path, struct kstatfs *buf)
 {
 	int error = 0;
-#ifdef CONFIG_KSU_SUSFS_OPEN_REDIRECT
-	struct inode *inode = path->dentry->d_inode;
-
-	if (SUSFS_IS_INODE_OPEN_REDIRECT(inode)) {
-		if (susfs_open_redirect_spoof_vfs_statfs(inode, buf))
-			goto orig_flow;
-		return 0;
-	}
-#endif // #ifdef CONFIG_KSU_SUSFS_OPEN_REDIRECT
 
 #ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
 	if (susfs_is_current_app_uid()) {
@@ -167,9 +154,6 @@ int vfs_statfs(const struct path *path, struct kstatfs *buf)
 #endif // #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
 
 	error = statfs_by_dentry(path->dentry, buf);
-#if defined(CONFIG_KSU_SUSFS_OPEN_REDIRECT)
-orig_flow:
-#endif // #if defined(CONFIG_KSU_SUSFS_OPEN_REDIRECT)
 
 	if (!error)
 		buf->f_flags = calculate_f_flags(path->mnt);
