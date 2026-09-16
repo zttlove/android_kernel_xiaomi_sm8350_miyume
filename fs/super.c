b2285fef5977ab9dbd68437dd46b2f3ec064c851
zttlove <115808960+zttlove@users.noreply.github.com>
2026-09-16 22:21:42 +0800

    Update super.c

--- fs/super.c
+++ fs/super.c
@@ -38,7 +38,14 @@
 #include <linux/user_namespace.h>
 #include <linux/fs_context.h>
 #include <uapi/linux/mount.h>
+#ifdef CONFIG_KSU_SUSFS
+#include <linux/susfs_def.h>
+#endif // #ifdef CONFIG_KSU_SUSFS
 #include "internal.h"
+#ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
+extern bool susfs_is_current_ksu_domain(void);
+extern struct static_key_true susfs_is_sdcard_android_data_not_decrypted;
+#endif // #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
 
 static int thaw_super_locked(struct super_block *sb);
 
@@ -1084,6 +1091,21 @@ static DEFINE_IDA(unnamed_dev_ida);
 int get_anon_bdev(dev_t *p)
 {
 	int dev;
+#ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
+	if (static_branch_unlikely(&susfs_is_sdcard_android_data_not_decrypted)) {
+		if (susfs_is_current_ksu_domain()) {
+			dev = ida_alloc_range(&unnamed_dev_ida, DEFAULT_KSU_MNT_MINOR_DEV, (1 << MINORBITS) - 1,
+				GFP_ATOMIC);
+			if (dev == -ENOSPC)
+				dev = -EMFILE;
+			if (dev < 0)
+				return dev;
+
+			*p = MKDEV(0, dev);
+			return 0;
+		}
+	}
+#endif // #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
 
 	/*
 	 * Many userspace utilities consider an FSID of 0 invalid.
