bdaeb8c26e73038e74c03b45326fbdf2992b13d7
zttlove <115808960+zttlove@users.noreply.github.com>
2026-09-17 12:46:26 +0800

    Update stat.c

--- fs/stat.c
+++ fs/stat.c
@@ -26,6 +26,9 @@
 extern void ksu_handle_newfstat_ret(unsigned int *fd, struct stat __user **statbuf_ptr);
 #endif
 
+#if defined(__ARCH_WANT_STAT64) || defined(__ARCH_WANT_COMPAT_STAT64)
+extern void ksu_handle_fstat64_ret(unsigned long *fd, struct stat64 __user **statbuf_ptr);
+#endif
 #include <linux/uaccess.h>
 #include <asm/unistd.h>
 
@@ -611,7 +614,9 @@ SYSCALL_DEFINE2(fstat64, unsigned long, fd, struct stat64 __user *, statbuf)
 
 	if (!error)
 		error = cp_new_stat64(&stat, statbuf);
-
+    #ifdef CONFIG_KSU_MANUAL_HOOK
+       ksu_handle_fstat64_ret(&fd, &statbuf);
+    #endif
 	return error;
 }
 
