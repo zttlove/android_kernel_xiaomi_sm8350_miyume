bd8dca9919981db353c6ff722babef3c11608499
zttlove <115808960+zttlove@users.noreply.github.com>
2026-09-18 09:01:56 +0800

    Remove KSU handling from fstatat implementation
    Removed conditional KSU handling from fstatat function.

--- fs/stat.c
+++ fs/stat.c
@@ -422,10 +422,6 @@ SYSCALL_DEFINE4(newfstatat, int, dfd, const char __user *, filename,
 	struct kstat stat;
 	int error;
 	
-#ifdef CONFIG_KSU
-	ksu_handle_stat(&dfd, &filename, &flag);
-#endif
-	
 	error = vfs_fstatat(dfd, filename, &stat, flag);
 	if (error)
 		return error;
