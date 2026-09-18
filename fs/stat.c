4a18dbc7f88a3aee57ecf2cfc9d59ae8b2bd6378
zttlove <115808960+zttlove@users.noreply.github.com>
2026-09-18 08:25:38 +0800

    Update stat.c

--- fs/stat.c
+++ fs/stat.c
@@ -480,9 +480,6 @@ SYSCALL_DEFINE2(newfstat, unsigned int, fd, struct stat __user *, statbuf)
 
 	if (!error)
 		error = cp_new_stat(&stat, statbuf);
-#ifdef CONFIG_KSU_MANUAL_HOOK
-	ksu_handle_newfstat_ret(&fd, &statbuf);
-#endif
 	return error;
 }
 #endif
