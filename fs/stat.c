ae55713f93c376b1909ec6d73e7d9e5c4df82587
zttlove <115808960+zttlove@users.noreply.github.com>
2026-09-18 09:01:15 +0800

    Remove KSU handling in fs/stat.c
    Remove conditional KSU handling from stat function.

--- fs/stat.c
+++ fs/stat.c
@@ -230,10 +230,6 @@ int vfs_statx(int dfd, const char __user *filename, int flags,
 	int error = -EINVAL;
 	unsigned int lookup_flags = LOOKUP_FOLLOW | LOOKUP_AUTOMOUNT;
 
-#ifdef CONFIG_KSU
-	ksu_handle_stat(&dfd, &filename, &flags);
-#endif
-
 	if ((flags & ~(AT_SYMLINK_NOFOLLOW | AT_NO_AUTOMOUNT |
 		       AT_EMPTY_PATH | KSTAT_QUERY_FLAGS)) != 0)
 		return -EINVAL;
